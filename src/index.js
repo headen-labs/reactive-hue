c02e445d70d86ecd7e4ad2f76d15a5cc00c35f62
Quentin Headen <quentin@headenlabs.com>
2018-10-27 11:03:53 -0400

    Add initial project structure

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+let greeting = <b>Hello World</b>;
+
+ReactDOM.render(
+    greeting,
+    document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src/index.template.html'),
+            xhtml: true
+        })
+    ]
+};
